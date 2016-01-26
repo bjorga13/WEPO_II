14a6e04326b54009183c8ee3648557d7569081bd
Erla14 <erla14@ru.is>
2016-01-26 20:11:46 +0000

    line fixed

--- JSDraw/Line.js
+++ JSDraw/Line.js
@@ -7,15 +7,15 @@ var Line = Shape.extend({
 	draw: function(canvas) {
 		canvas.beginPath();
 		canvas.strokeStyle = this.color;
-		canvas.moveTo(this.pos.x, this.pos.y);
-		canvas.lineTo(this.size.x, this.size.y);
+		canvas.moveTo(this.size.x, this.size.y);
+		canvas.lineTo(this.pos.x, this.pos.y);
 		
 		canvas.stroke();
 	},
 
 	drawing:function(point) {
-		this.size.x = point.x - this.pos.x;
-		this.size.y = point.y - this.pos.y;
+		this.size.x = point.x;
+		this.size.y = point.y;
 	},
 
 	added: function(canvas) {
