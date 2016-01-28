6cab4f42ae6f7f1b8ad248a102bcd8b5e153727d
Erla14 <erla14@ru.is>
2016-01-28 19:22:51 +0000

    pen almost there

--- JSDraw/Pen.js
+++ JSDraw/Pen.js
@@ -2,36 +2,37 @@ var Pen = Shape.extend({
 
 	constructor: function() {
 		this.base("Pen");
+		this.path = new Array();
 	},
-	
 	draw: function(canvas) {
 
-		var radius = 2;
-		canvas.lineWidth = radius * 2;
-		canvas.lineTo(this.pos.x, this.pos.y);
-		canvas.stroke();
+		canvas.strokeStyle = this.color;
 		canvas.beginPath();
-		canvas.arc(this.pos.x, this.pos.y, radius, Math.PI*2, false);
-		canvas.fill();
-		canvas.beginPath();
-		canvas.moveTo(this.size.x, this.size.y);		
+		
+		canvas.moveTo(this.path[0].x + this.pos.x, this.path[0].y + this.pos.y);
+
+		for (var i = 1; i < this.path.length; i++) {
+			canvas.lineTo(this.path[i].x + this.pos.x, this.path[i].y + this.pos.y);
+		}
+		this.base(canvas);
+		canvas.stroke();	
+
 	},
 
 	drawing:function(point) {
-		this.size.x = point.x;
-		this.size.y = point.y;
+		this.path.push(point.substract(this.pos));
 	},
 
 	added: function(canvas) {
-		if(this.size.x < 0) {
-			this.pos.x += this.size.x;
-			this.size.x = Math.abs(this.size.x);
-		}
+		var p = new Point(this.pos.x, this.pos.y);
+		this.path.push(this.pos);
 
-		if(this.size.y < 0) {
-			this.pos.y += this.size.y;
-			this.size.y = Math.abs(this.size.y);
-		}
 	},	
+	drawingStop:function(point) {
+		this.path.push(point.substract(this.pos));
+	},
+	drawingStart:function(point) {
+		this.path.push(point.substract(this.pos));
+	}
 
 });
